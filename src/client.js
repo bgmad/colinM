aec819ce618d5bc15d5b34bee41703e729b505a5
Ian Luchini <iancarloluchini@gmail.com>
2021-12-17 03:18:27 -0500

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch feauter/fetchingData # Changes to be committed: #	modified:   package-lock.json #	modified:   package.json Added `contentful` dependency. #	new file:   src/client.js Created this `Client` which connects to Contentful. **Note: These strings should be set up with dotenv!!** #

---
+++ src/client.js
@@ -0,0 +1,6 @@
+import * as contentful from 'contentful';
+
+export const client = contentful.createClient({
+    space: 'k7u27vv4uvnf',
+    accessToken: 'BMD7d07qWh3ZHg5fy1vYRli3SsNRzAmuy2nUfRgezgA' 
+});
