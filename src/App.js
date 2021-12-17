3336be33043b0f78f0b72c53fb273fd77b7a5ea4
Ian Luchini <iancarloluchini@gmail.com>
2021-12-17 03:06:48 -0500

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch feature/background # Changes to be committed: #	modified:   src/App.css Decided that I didn't want to set the view height. #	modified:   src/App.js Noticed that the `Title` was pointing to a `<Title>` html tag instead of `<h1>` #

--- src/App.js
+++ src/App.js
@@ -2,22 +2,15 @@ import './App.css';
 import Background from './components/Background';
 import styled from 'styled-components';
 
-const Title = styled.Title`
+const Title = styled.h1`
   color: white;
   font-size: 4rem;
   z-index: 10;
 `;
 
-const Square = styled.div`
-  background: red;
-  width: 50px;
-  height: 50px;
-`;
-
 function App() {
   return (
     <div className="App">
-      <Square />
       <Title>Colin J. Murray</Title>
       <Background />  
     </div>
