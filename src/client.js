63b6db4743abef4fc362e4f98f5d0ba881df69d1
Ian Luchini <iancarloluchini@gmail.com>
2021-12-17 13:27:26 -0500

    Added dotenv support for spaceId and accessToken.
    Created .env file to hold sensitive information and called it in the client.js function.

--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
 import * as contentful from 'contentful';
 
 export const client = contentful.createClient({
-    space: 'k7u27vv4uvnf',
-    accessToken: 'BMD7d07qWh3ZHg5fy1vYRli3SsNRzAmuy2nUfRgezgA' 
+    space: process.env.REACT_APP_SPACE_ID,
+    accessToken: process.env.REACT_APP_ACCESS_TOKEN 
 });
